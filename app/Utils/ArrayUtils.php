e29ec7307167cc487840057e56225bf0f9700387
kukuhpr21 <kukuhpr21@gmail.com>
2024-12-03 16:29:21 +0700

    init arrayutils

---
+++ app/Utils/ArrayUtils.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Utils;
+
+trait ArrayUtils
+{
+    public static function mapping($source, $maps)
+    {
+        $keys = array_keys($maps);
+
+        foreach ($source as $item) {
+            # code...
+        }
+    }
+}
