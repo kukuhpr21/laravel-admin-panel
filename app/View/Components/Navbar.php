023da5d50ef78f789de7eb84e256fe76929d3fc3
Kukuh182 <kukuhpr21@gmail.com>
2024-12-05 06:51:50 +0700

    init navbar

---
+++ app/View/Components/Navbar.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\View\Components;
+
+use App\Utils\SessionUtils;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Navbar extends Component
+{
+    private SessionUtils $sessionUtils;
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        $this->sessionUtils = new SessionUtils();
+        $name =  $this->sessionUtils->get('name');
+        $role =  json_decode($this->sessionUtils->get('role'), true)['name'];
+
+        $data = [
+            $name,
+            $role
+        ];
+
+        dd($data);
+        return view('components.navbar');
+    }
+}
