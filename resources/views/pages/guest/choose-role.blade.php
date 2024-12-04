35aad22d1012213ebc0bc84969abd76d0352e8ac
kukuhpr21 <kukuhpr21@gmail.com>
2024-12-04 16:12:55 +0700

    init form choose role

--- resources/views/pages/guest/choose-role.blade.php
+++ resources/views/pages/guest/choose-role.blade.php
@@ -5,15 +5,7 @@
     <div class="flex justify-center w-full">
         <x-logo :showDesc="false" />
     </div>
-    <x-input type="email" name="Email"/>
-    <x-input type="password" name="Password" placeholder=". . . . . ."/>
-    <x-button type="submit" color="gray" size="lg" :full="true">Log In</x-button>
+    <x-select name="Role" :data="$roles"/>
+    <x-button type="submit" color="gray" size="lg" :full="true">Submit</x-button>
 </x-form>
 @endsection
-<form wire:submit="submit" class="flex flex-col lg:w-1/4 w-full m-5 bg-white p-8 rounded-2xl drop-shadow-2xl shadow-white">
-    <div class="flex justify-center w-full">
-        <x-logo showDesc="false" />
-    </div>
-    <x-select name="Role" val_default_select="Pilih Role" :items="$roles" :form="true"/>
-    <x-button type="submit" color="gray" size="lg" :full="true">Submit</x-button>
-</form>
