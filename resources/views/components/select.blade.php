538c00041e0efaf2f0c8f5a3e4338eae73ba075e
kukuhpr21 <kukuhpr21@gmail.com>
2024-12-04 16:12:22 +0700

    update select component

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -2,21 +2,16 @@
     'name',
     'data',
     'color' => 'slate',
-    'value' => '',
+    'valueSelected' => '',
     'label' => true,
     'withInvalidInput' => true,
     'desc' => '',
-    'showOldValue' => true,
 ])
 
 @php
     $lowerName = str_replace(" ", "_", strtolower($name));
     $baseColor = "bg-".$color."-50 focus:bg-".$color."-50";
-    $classes = $baseColor.' select2 py-4 px-3 my-3 rounded-2xl border border-slate-300 focus:outline-none focus:ring-0 hover:drop-shadow-xl focus:drop-shadow-xl placeholder:italic';
-    $setValue = old($lowerName);
-    if (!empty($value)) {
-        $setValue = $value;
-    }
+    $classes   = $baseColor.' select2';
 @endphp
 
 <div class="flex flex-col">
@@ -26,7 +21,17 @@
 
     <select name="{{ $lowerName }}" id="{{ $lowerName }}" class="{{ $classes }}">
         @foreach ($data as $item)
-            <option value=""></option>
+            <option value="{{ $item['value'] }}" {{ (!empty($valueSelected) && $item['value'] == $valueSelected) ? 'selected' : '' }}>{{ $item['text'] }}</option>
         @endforeach
     </select>
+
+    @if (!empty($desc))
+        <x-description-input>{{ $desc }}</x-description-input>
+    @endif
+
+    @if ($withInvalidInput)
+        @error($lowerName)
+            <x-invalid-input>{{$message}}</x-invalid-input>
+        @enderror
+    @endif
 </div>
