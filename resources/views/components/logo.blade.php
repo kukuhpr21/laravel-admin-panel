1289c50743f7a708556d9caa9d802768ae87c034
Kukuh182 <kukuhpr21@gmail.com>
2024-12-06 05:57:19 +0700

    handle long name logo

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -4,7 +4,7 @@
     $letter = strtoupper(substr($name, 0, 1));
     $show = $showDesc ? '' : 'hidden';
 @endphp
-<div class="flex flex-row items-end p-2">
+<div class="flex flex-row items-center p-2">
     <span class="font-bold text-xl text-white bg-gray-600 rounded-md px-4 py-2">{{ $letter }}</span>
-    <span class="font-semibold text-xl text-gray-600 pl-2 {{ $show }}">{{ $name }}</span>
+    <span class="font-semibold text-lg text-gray-600 pl-2 overflow-hidden text-ellipsis whitespace-normal max-h-[4rem] line-clamp-2 {{ $show }}">{{ $name }}</span>
 </div>
