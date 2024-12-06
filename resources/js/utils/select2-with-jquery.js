57684f36bcf2d24d99ec5f5e33e738f7f86bf7d7
Kukuh182 <kukuhpr21@gmail.com>
2024-12-06 22:33:00 +0700

    remove unused

--- resources/js/utils/select2-with-jquery.js
+++ resources/js/utils/select2-with-jquery.js
@@ -8,9 +8,4 @@ select2($);
 
 $(document).ready(function () {
     $('.select2').select2();
- // Apply Tailwind classes after initialization
-//  $('.select2-container').addClass('w-full');
-//  $('.select2-selection--single').addClass('bg-gray-50 border border-gray-300 rounded-lg shadow-sm');
-//  $('.select2-results__option').addClass('px-4 py-2 cursor-pointer text-gray-800');
-//  $('.select2-results__option--highlighted').addClass('bg-blue-500 text-white');
 });
