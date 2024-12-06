bdc17c336130d2f29cfb251a4f291afd35a0a14d
Kukuh182 <kukuhpr21@gmail.com>
2024-12-06 19:46:41 +0700

    update card component

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -6,9 +6,10 @@
     'description' => 'Description'
 ])
 @php
-    $color = `bg-$color-100`;
+    $baseClasses = "flex flex-col gap-3 py-7 px-5 rounded-lg hover:drop-shadow-xl";
+    $colorClasses = "bg-$color-100";
 @endphp
-<div class="{{ $color }} flex flex-col gap-3 py-7 px-5 rounded-lg hover:drop-shadow-xl" {{ $attributes }}>
+<div {{ $attributes->merge(['class' => "{$baseClasses} {$colorClasses}"])}}>
     @switch($type)
         @case('custom')
             <span class="text-lg text-gray-600 font-normal">{{ $title }}</span>
@@ -17,7 +18,7 @@
             @break
         @default
             <span class="text-lg text-gray-600 font-normal">{{ $title }}</span>
-            <span class="text-2xl text-gray-600 font-semibold">{{ $slot }}</span>
+            <span class="text-2xl text-gray-600 font-semibold">{{ $body }}</span>
             <span class="text-sm text-gray-400 font-normal">{{ $description }}</span>
             @break
     @endswitch
