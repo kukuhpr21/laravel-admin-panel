1ef434e8f992845263a18df6ec9710b0a49f8760
Kukuh182 <kukuhpr21@gmail.com>
2024-12-06 07:19:53 +0700

    update dashboard

--- resources/views/pages/app/dashboard/index.blade.php
+++ resources/views/pages/app/dashboard/index.blade.php
@@ -2,6 +2,6 @@
 @section('title', 'Dashboard')
 @section('content')
 <x-page title="Dashboard">
-
+    <x-card color="teal" title="Total Pemasukan" body="100" description=""/>
 </x-page>
 @endsection
