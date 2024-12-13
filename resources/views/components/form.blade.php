c7813bf4b2e5bc6670b6087161e5371589952975
Kukuh182 <kukuhpr21@gmail.com>
2024-12-14 05:38:33 +0700

    enhancment form component

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,5 +1,8 @@
-@props(['routeName', 'class' => '', 'csrf' => true])
-<form action="{{ route($routeName) }}" method="POST" class="flex flex-col gap-3 py-3 px-4 bg-blue-50 rounded-2xl drop-shadow-2xl shadow-white w-full {{ $class }}">
+@props(['routeName', 'class' => '', 'shadow' => 'drop-shadow-2xl', 'csrf' => true])
+@php
+    $baseClasses = "flex flex-col gap-3 py-3 px-4 bg-blue-50 rounded-2xl shadow-white w-full"
+@endphp
+<form action="{{ route($routeName) }}" method="POST" {{ $attributes->merge(['class' => "{$baseClasses} {$class} {$shadow}"])}}>
     @if ($csrf)
         @csrf
     @endif
