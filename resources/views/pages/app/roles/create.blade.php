12686a32548e4be7eaae043eda8a20cf6c2ad249
Kukuh182 <kukuhpr21@gmail.com>
2024-12-14 08:25:55 +0700

    add create role

--- resources/views/pages/app/roles/create.blade.php
+++ resources/views/pages/app/roles/create.blade.php
@@ -2,5 +2,11 @@
 @section('title', 'Tambah Role')
 @section('content')
 <x-page title="Tambah Role" :back="true" routeBack="roles">
+    <x-form routeName="roles-add" class="sm:w-1/2" shadow="drop-shadow-md">
+        <x-input type="text" name="Name"/>
+        <div class="flex w-full justify-end">
+            <x-button type="submit" color="gray" size="md">Submit</x-button>
+        </div>
+    </x-form>
 </x-page>
 @endsection
