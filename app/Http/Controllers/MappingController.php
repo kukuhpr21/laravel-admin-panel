8e7206ea687b24b525c7cbc28eaab810d3999c15
Kukuh182 <kukuhpr21@gmail.com>
2024-12-20 07:54:15 +0700

    init mapping menu permission

--- app/Http/Controllers/MappingController.php
+++ app/Http/Controllers/MappingController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class MappingController extends Controller
 {
-    //
+    public function menuPermission()
+    {
+        return view('pages.app.mappings.menu-permission');
+    }
 }
---
+++ resources/views/pages/app/mappings/menu-permission.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', 'Mapping Menu Permission')
+@section('content')
+<x-page title="Mapping Menu Permission">
+
+</x-page>
+@endsection
