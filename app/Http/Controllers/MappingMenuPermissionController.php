eee13e45d1468f021d83d66eebad25a10a3b4f8d
Kukuh182 <kukuhpr21@gmail.com>
2024-12-21 23:26:47 +0700

    add list mapping menu permission

--- app/Http/Controllers/MappingMenuPermissionController.php
+++ app/Http/Controllers/MappingMenuPermissionController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\DataTables\MenuHasPermissionDataTable;
 
 class MappingMenuPermissionController extends Controller
 {
-    //
+    public function index(MenuHasPermissionDataTable $dataTable)
+    {
+        return $dataTable->render('pages.app.mappings.menuspermissions.list');
+    }
+
+    public function create()
+    {
+        return view('pages.app.mappings.menuspermissions.create');
+    }
+
+    public function store()
+    {
+
+    }
 }
--- resources/views/pages/app/mappings/menuspermissions/list.blade.php
+++ resources/views/pages/app/mappings/menuspermissions/list.blade.php
@@ -2,6 +2,6 @@
 @section('title', 'Daftar Mapping Menu Permission')
 @section('content')
 <x-page title="Daftar Mapping Menu Permission">
-
+    <x-table :showButtonAdd="true" routeButtonAdd="{{ route('menus-permissions-add') }}" :data="$dataTable"/>
 </x-page>
 @endsection
