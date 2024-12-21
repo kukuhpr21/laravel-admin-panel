9ca8566871032ac4ce64d377cbf3d19dc6dc389a
Kukuh182 <kukuhpr21@gmail.com>
2024-12-21 23:27:32 +0700

    init form create and edit

---
+++ resources/views/pages/app/mappings/menuspermissions/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('title', 'Tambah Role')
+@section('content')
+<x-page title="Tambah Mapping Menu Permission" :back="true" routeBack="{{ route('menus-permissions') }}">
+    <x-form routeName="{{ route('menus-permissions-add') }}" class="sm:w-1/2" shadow="drop-shadow-md">
+        <x-input type="text" name="Name"/>
+        <div class="flex w-full justify-end">
+            <x-button type="submit" color="gray" size="md">Submit</x-button>
+        </div>
+    </x-form>
+</x-page>
+@endsection
