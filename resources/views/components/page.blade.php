82a74d338f5b82f8c222726408686177a65cd15c
Kukuh182 <kukuhpr21@gmail.com>
2024-12-21 15:05:07 +0700

    update href routeback

--- resources/views/components/page.blade.php
+++ resources/views/components/page.blade.php
@@ -2,7 +2,7 @@
 <div class="flex flex-col w-full h-screen overflow-auto py-6 px-4 bg-white rounded-2xl drop-shadow-md shadow-black scrollbar-hide">
     <div class="flex flex-row gap-2 items-center">
         @if ($back)
-            <a href="{{ route($routeBack) }}" class="hover:bg-slate-200 hover:drop-shadow-sm hover:rounded-lg my-2 py-1 px-2">
+            <a href="{{ $routeBack }}" class="hover:bg-slate-200 hover:drop-shadow-sm hover:rounded-lg my-2 py-1 px-2">
                 <i class="ri-arrow-left-s-line ri-2x text-slate-600"></i>
             </a>
         @endif
