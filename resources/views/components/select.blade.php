f13a67dd38e2122add909ae6c127d9f166df1c84
Kukuh182 <kukuhpr21@gmail.com>
2024-12-22 14:29:24 +0700

    update select2

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -6,12 +6,14 @@
     'label' => true,
     'withInvalidInput' => true,
     'desc' => '',
+    'multiple' => false,
 ])
 
 @php
     $lowerName = str_replace(" ", "_", strtolower($name));
     $baseColor = "bg-".$color."-50 focus:bg-".$color."-50";
     $classes   = $baseColor.' select2';
+    $multiple  = $multiple ? 'multiple' : '';
 @endphp
 
 <div class="flex flex-col">
@@ -19,7 +21,7 @@
         <x-label :name="$name" />
     @endif
 
-    <select name="{{ $lowerName }}" id="{{ $lowerName }}" class="{{ $classes }}">
+    <select name="{{ $lowerName }}" id="{{ $lowerName }}" class="{{ $classes }}" {{ $multiple }}>
         @if (count($data) > 0)
             @foreach ($data as $item)
                 <option value="{{ $item['value'] }}" {{ (!empty($valueSelected) && $item['value'] == $valueSelected) ? 'selected' : '' }}>{{ $item['text'] }}</option>
