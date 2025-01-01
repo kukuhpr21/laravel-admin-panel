1d1e7eb4e5a58f7922ed9301321b432f1f9bc580
Kukuh182 <kukuhpr21@gmail.com>
2025-01-02 06:46:58 +0700

    update sidebar

--- app/View/Components/Sidebar.php
+++ app/View/Components/Sidebar.php
@@ -7,6 +7,7 @@ use App\Utils\SessionUtils;
 use App\Services\MenuService;
 use Illuminate\View\Component;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Log;
 
 class Sidebar extends Component
 {
@@ -34,7 +35,9 @@ class Sidebar extends Component
         $tree = '';
 
         foreach ($menus as $item) {
-            $isActive = implode('/', $segments) === $item['link'] ? 'bg-gray-200' : '';
+            $link         = implode('/', $segments);
+            $isActive     = $link == $item['link'] || $link == $this->findLink($link, $item['children']);
+            $bgSelectMenu = $isActive ? 'bg-gray-200' : '';
 
             $icon = '';
 
@@ -43,21 +46,25 @@ class Sidebar extends Component
             }
 
             if ($item['children']) {
-                $id = $item['id'].'-accordion';
-                $tree .= '<li class="hs-accordion" id="'.$id.'">';
-                $tree .= '<button type="button" class="hs-accordion-toggle '. $isActive .' hs-accordion-active:text-blue-600 hs-accordion-active:hover:bg-transparent w-full text-start flex items-center gap-x-3.5 py-2 px-3 text-sm text-gray-700 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100" aria-expanded="true" aria-controls="'.$id.'">';
+                $hsAccordionIsActive = $isActive ? 'active' : '';
+                $ariaExpanded        = $isActive ? 'true' : 'false';
+                $styleChildren       = $isActive ? 'display: block;' : 'height: 0px;';
+                $id                  = $item['id'].'-accordion';
+
+                $tree .= '<li class="hs-accordion '.$hsAccordionIsActive.'" id="'.$id.'">';
+                $tree .= '<button type="button" aria-expanded="'.$ariaExpanded.'" class="hs-accordion-toggle '. $bgSelectMenu .' hs-accordion-active:text-blue-600 hs-accordion-active:hover:bg-transparent w-full text-start flex items-center gap-x-3.5 py-2 px-3 text-sm text-gray-700 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100" aria-expanded="true" aria-controls="'.$id.'">';
                 $tree .= $icon;
                 $tree .= $item['name'];
                 $tree .= '<svg class="hs-accordion-active:block ms-auto hidden size-4 text-gray-600 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m18 15-6-6-6 6"/></svg>';
                 $tree .= '<svg class="hs-accordion-active:hidden ms-auto block size-4 text-gray-600 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m6 9 6 6 6-6"/></svg>';
                 $tree .= '</button>';
-                $tree .= '<div id="'.$id.'" class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300 hidden" role="region" aria-labelledby="'.$id.'">';
+                $tree .= '<div id="'.$id.'" class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300 hidden" role="region" aria-labelledby="'.$id.'" style="'.$styleChildren.'">';
                 $tree .= '<ul class="hs-accordion-group p-2 gap-2" data-hs-accordion-always-open>';
                 $tree .= self::makeHTMLSiderbar($item['children'], $segments);
                 $tree .= '</ul>';
             } else {
                 $tree .= '<li class="my-2">';
-                $tree .= '<a wire:navigate class="flex items-center gap-x-3.5 py-2 px-3 '. $isActive .' text-sm text-gray-700 rounded-lg hover:bg-gray-100" href="'.route($item['link_alias']).'">';
+                $tree .= '<a class="flex items-center gap-x-3.5 py-2 px-3 '. $bgSelectMenu .' text-sm text-gray-700 rounded-lg hover:bg-gray-100" href="'.route($item['link_alias']).'">';
                 $tree .= $icon;
                 $tree .= $item['name'];
                 $tree .= '</a>';
@@ -66,5 +73,18 @@ class Sidebar extends Component
            $tree .= '</li>';
         }
         return $tree;
+    }
+
+    private function findLink($path, $menus) {
+        foreach ($menus as $menu) {
+            if (isset($menu['link']) && $menu['link'] === ltrim($path, '/')) {
+                return true;
+            }
+            if (isset($menu['children']) && is_array($menu['children'])) {
+                if (self::findLink( $path,  $menu['children'])){
+                    return $path;
+                    }
+            }
+        }
     }
 }
