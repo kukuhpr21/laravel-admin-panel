2d52fa7d9c008ed4f2e9a321053b24ae0190cc33
kukuhpr21 <kukuhpr21@gmail.com>
2025-01-14 14:31:22 +0700

    add favicon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <x-favicon/>
         <title>
             @if(View::hasSection('title'))
                 @yield('title') | {{ config('app.name')}}
