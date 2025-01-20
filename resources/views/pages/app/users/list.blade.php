159295b6e125bf58fc0e74c10b097ed1e5fb6c85
kukuhpr21 <kukuhpr21@gmail.com>
2025-01-20 12:04:51 +0700

    update filter list user

--- resources/views/pages/app/users/list.blade.php
+++ resources/views/pages/app/users/list.blade.php
@@ -2,17 +2,11 @@
 @section('title', 'Daftar User')
 @section('content')
 <x-page title="Daftar User">
-    <x-form routeName="{{ route('users') }}" class="sm:w-1/2 mb-4" shadow="drop-shadow-md">
-        <x-label name="Filter"/>
-        <div id="section_filter" class="grid grid-cols-1 sm:grid-cols-3 gap-3">
-            <x-select name="Status" :data="$statuses" :valueSelected="$status"/>
-            <x-select name="Role" :data="$roles" :valueSelected="$role"/>
-            <x-input type="date" name="Created At" :value="$createdAt"/>
-        </div>
-        <div class="flex w-full justify-end">
-            <x-button type="submit" color="teal" size="md">Filter</x-button>
-        </div>
-    </x-form>
+    <x-form-filter routeName="{{ route('users') }}" classForm="sm:w-1/2 mb-4" classFilter="grid grid-cols-1 sm:grid-cols-3 gap-3" shadow="drop-shadow-md">
+        <x-select name="Status" :data="$statuses" :valueSelected="$status"/>
+        <x-select name="Role" :data="$roles" :valueSelected="$role"/>
+        <x-input type="date" name="Created At" :value="$createdAt"/>
+    </x-form-filter>
     <x-table :showButtonAdd="true" routeButtonAdd="{{ route('users-add') }}" :data="$dataTable"/>
 </x-page>
 @endsection
