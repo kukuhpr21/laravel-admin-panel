891d44020e4c00719b410cf9983d9e5dda99ccd6
kukuhpr21 <kukuhpr21@gmail.com>
2025-01-21 10:18:21 +0700

    update list user

--- resources/views/pages/app/users/list.blade.php
+++ resources/views/pages/app/users/list.blade.php
@@ -2,12 +2,13 @@
 @section('title', 'Daftar User')
 @section('content')
 <x-page title="Daftar User">
-    <x-form-filter routeName="{{ route('users') }}" classForm="sm:w-1/2 mb-4" classFilter="grid grid-cols-1 sm:grid-cols-3 gap-3" shadow="drop-shadow-md">
-        <x-select name="Status" :data="$statuses" :valueSelected="$status"/>
-        <x-select name="Role" :data="$roles" :valueSelected="$role"/>
-        <x-input type="date" name="Created At" :value="$createdAt"/>
-    </x-form-filter>
-    <x-table :showButtonAdd="true" routeButtonAdd="{{ route('users-add') }}" :data="$dataTable"/>
+    <x-table :showButtonAdd="true" routeButtonAdd="{{ route('users-add') }}" :data="$dataTable">
+        <x-form-filter routeName="{{ route('users') }}" classForm="sm:w-1/2 mb-4" classFilter="grid grid-cols-1 sm:grid-cols-3 gap-3" shadow="drop-shadow-md">
+            <x-select name="Status" :data="$statuses" :valueSelected="$status"/>
+            <x-select name="Role" :data="$roles" :valueSelected="$role"/>
+            <x-input type="date" name="Created At" :value="$createdAt"/>
+        </x-form-filter>
+    </x-table>
 </x-page>
 @endsection
 @push('scripts')
